a45ffa277edbe1cc2c5c1cccb7f99bda641981f9
yashpalsaini <yash1628413@gmail.com>
2014-08-24 12:30:09 +0530

    Update default.php

--- default.php
+++ default.php
@@ -1,1 +1,10 @@
-check
+<?php
+/**
+ * Plugin Name:  n66 wp comment filter 
+ * Description: check comments for spam and replace some words
+ * Version: 1.0.0
+ * Author: Yashpal Saini
+ * Author URI: http://www.spysaini.wordpress.com/
+ */
+
+
